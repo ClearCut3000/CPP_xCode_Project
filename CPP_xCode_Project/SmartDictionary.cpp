b647639eb25c042b543fe41f1fec54ad50b24a9e
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2025-04-21 20:20:44 +0300

    added smart dictionary project

---
+++ CPP_xCode_Project/SmartDictionary.cpp
@@ -0,0 +1,8 @@
+//
+//  SmartDictionary.cpp
+//  CPP_xCode_Project
+//
+//  Created by Николай Никитин on 21.04.2025.
+//
+
+#include "SmartDictionary.hpp"
---
+++ CPP_xCode_Project/SmartDictionary.hpp
@@ -0,0 +1,13 @@
+//
+//  SmartDictionary.hpp
+//  CPP_xCode_Project
+//
+//  Created by Николай Никитин on 21.04.2025.
+//
+
+#ifndef SmartDictionary_hpp
+#define SmartDictionary_hpp
+
+#include <stdio.h>
+
+#endif /* SmartDictionary_hpp */
